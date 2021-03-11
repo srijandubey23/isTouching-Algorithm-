bfa489d32a58069149649b75061f15fba45a0f8f
srijandubey23 <79911091+srijandubey23@users.noreply.github.com>
2021-03-11 16:56:23 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,6 +10,7 @@ function draw() {
   background(255,255,255);  
   movingRect.x=World.mouseX;
   movingRect.y=World.mouseY;
+  //concept of isTouching
 if(movingRect.x-fixedRect.x<fixedRect.width/2+movingRect.width/2  && 
   fixedRect.x-movingRect.x<fixedRect.width/2+movingRect.width/2  &&
   movingRect.y-fixedRect.y<fixedRect.height/2+movingRect.height/2  &&
